51ece4fab2fddb8fde167aed128d1d93d1455788
Ernesto Celis <developer@celisdelafuente.net>
2013-11-15 10:14:13 -0600

    * README updated for sauron-com-kit.

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ make distclean
 ./configure --disable-video --disable-ffmpeg --disable-v4l2
 CFLAGS="-fPIC" CXXFLAGS="-fPIC" make dep
 CFLAGS="-fPIC" CXXFLAGS="-fPIC" make 
+cd pjsip-apps/src/python
+python setup.py build
+
