1862ce8ff8f0cd5d6e4510e6500858dcf0571e4b
Ernesto Celis <developer@celisdelafuente.net>
2013-12-10 18:32:18 -0600

    * WIP audio mixer

--- ve-phone/vess.py
+++ ve-phone/vess.py
@@ -14,9 +14,21 @@
 #
 import pyalsa.alsacard as acard
 import pyalsa.alsamixer as amixer
-import syslog
+import sys
 
 class VSS:
+    def __init__(self):
+	if sys.version_info[0] == 2:
+	    if sys.version_info[1] > 4:
+		# TODO Check if it works with 2.5 and 2.6
+		self.impl = _VSS27()
+	    elif sys.version_info[1] == 4:
+		self.impl = _VSS24()
+	    else:
+	        # TODO log here
+	        sys.exit(1)
+
+class _VSS24:
     def __init__(self):
 	# Get the mixer
 	self.mixer = amixer.Mixer()
@@ -48,8 +60,81 @@ class VSS:
     def _set_capture(self, element):
 	self.capture = amixer.Element(self.mixer, element[0], element[1])
 
+    #def _set_capture(self, element):
+    #	self.capture = amixer.Element(self.mixer, element[0], element[1])
+
+    def mute_all(self):
+	if self.master:
+            self.master.setVolumeAll(0)
+
+    def mute_mic(self, state=True):
+	pass
+
+    def mute_phone(self, state=True):
+	pass
+
+    def mute_speaker(self, state=True):
+	pass
+
+    def open_mic(self):
+	pass
+
+    def stereo_on(self, state=False):
+	pass
+
+    def migGain_down(self):
+	pass
+
+    def micGain_up(self):
+	pass
+
+    def phoneVol_up(self):
+	pass
+
+    def phoneVol_down(self):
+        pass
+
+    def speakerVol_up(self):
+	pass
+
+    def speakerVol_down(self):
+	pass
+
+
+class _VSS27:
+    def __init__(self):
+	# Get the mixer
+	self.mixer = amixer.Mixer()
+	self.mixer.attach()
+	self.mixer.load()
+	self._get_mixer_elements()
+	self.mute_all()
+	
+    def _get_mixer_elements(self):
+	# Get the mixer elements
+	if self.mixer.count > 0:
+	    elements = self.mixer.list()
+	    for i in elements:
+                if i[0] == 'Master':
+	            self._set_master(i)
+		elif i[0] == 'PCM':
+		    self._set_pcm(i)
+		elif i[0] == 'Capture':
+		    self._set_capture(i)
+
+	# TODO mic & capture
+
+    def _set_master(self, element):
+	self.master = amixer.Element(self.mixer, element[0], element[1])
+
+    def _set_pcm(self, element):
+	self.pcm = amixer.Element(self.mixer, element[0], element[1])
+
     def _set_capture(self, element):
 	self.capture = amixer.Element(self.mixer, element[0], element[1])
+
+    #def _set_capture(self, element):
+    #	self.capture = amixer.Element(self.mixer, element[0], element[1])
 
     def mute_all(self):
 	if self.master:
