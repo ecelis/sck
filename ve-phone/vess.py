ac658ac0eed32b160ddbc15ba835b329db93087d
Ernesto Celis <developer@celisdelafuente.net>
2013-12-11 03:06:48 -0600

    * WIP audio mixer * UPDATE veconfig.py and config.ini.orig to include audio settings

--- ve-phone/vess.py
+++ ve-phone/vess.py
@@ -26,33 +26,42 @@ class VSS:
 	    elif sys.version_info[1] == 4:
 		self.impl = _VSS24()
 	    else:
-	        # TODO log here
+	        
 	        sys.exit(1)
 
-class _VSS24:
+class _Vss:
     def __init__(self):
-	# Get the mixer
+	self.audiocfg = veconfig.get_audiocfg()
+       # Get the mixer
 	self.mixer = amixer.Mixer()
 	self.mixer.attach()
 	self.mixer.load()
 	#print self.mixer.list()
 	self._get_mixer_elements()
 	self.mute_all()
-	
+
     def _get_mixer_elements(self):
 	# Get the mixer elements
-	audiocfg = veconfig.get_audiocfg()
 	if self.mixer.count > 0:
 	    elements = self.mixer.list()
 	    for i in elements:
-                if i[0] == 'Master':
-	            self._set_master(i)
-		elif i[0] == 'PCM':
-		    self._set_pcm(i)
-		elif i[0] == 'Capture':
-		    self._set_capture(i)
-
-	# TODO mic & capture
+		if i[0] == self.audiocfg['master']:
+		    self._set_master(i)
+		#elif i[0] == audiocfg['pcm']:
+		#    self._set_pcm(i)
+		#elif i[0] == audiocfg['capture']:
+		#    self._set_capture(i)
+		#elif i[0] == audiocfg['cap_idx']:
+		#    self._set_cap_idx(i)
+		#elif i[0] == audiocfg['input_src']:
+		#    self._set_input_src(i)
+		#elif i[0] == audiocfg['in_idx']:
+		#    self._set_in_idx(i)
+		#elif i[0] == audiocfg['mic']:
+		#    self._set_mic(i)
+		#elif i[0] == audiocfg['mic_boost']:
+		#    self._set_mic_boost(i)
+		pass
 
     def _set_master(self, element):
 	self.master = amixer.Element(self.mixer, element[0], element[1])
@@ -63,128 +72,66 @@ class _VSS24:
     def _set_capture(self, element):
 	self.capture = amixer.Element(self.mixer, element[0], element[1])
 
-    #def _set_capture(self, element):
-    #	self.capture = amixer.Element(self.mixer, element[0], element[1])
+    def _set_cap_idx(self, element):
+	self.capture = amixer.Element(self.mixer, element[0], element[1])
+
+    def _set_input_src(self, element):
+	self.capture = amixer.Element(self.mixer, element[0], element[1])
+
+    def mute_all(self):
+	pass
+
+    def mute_mic(self, state=True):
+	pass
+
+    def mute_phone(self, state=True):
+	pass
+
+    def mute_speaker(self, state=True):
+	pass
+
+    def open_mic(self):
+	pass
+
+    def stereo_on(self, state=False):
+	pass
+
+    def migGain_down(self):
+	pass
+
+    def micGain_up(self):
+	pass
+
+    def phoneVol_up(self):
+	pass
+
+    def phoneVol_down(self):
+        pass
+
+    def speakerVol_up(self):
+	pass
+
+    def speakerVol_down(self):
+	pass
+
+
+
+class _VSS24(_Vss):
+    def __init__(self):
+	_Vss.__init__(self)
 
     def mute_all(self):
 	if self.master:
             self.master.setVolumeAll(0)
 
-    def mute_mic(self, state=True):
-	pass
+   
 
-    def mute_phone(self, state=True):
-	pass
-
-    def mute_speaker(self, state=True):
-	pass
-
-    def open_mic(self):
-	pass
-
-    def stereo_on(self, state=False):
-	pass
-
-    def migGain_down(self):
-	pass
-
-    def micGain_up(self):
-	pass
-
-    def phoneVol_up(self):
-	pass
-
-    def phoneVol_down(self):
-        pass
-
-    def speakerVol_up(self):
-	pass
-
-    def speakerVol_down(self):
-	pass
-
-
-class _VSS27:
+class _VSS27(_Vss):
     def __init__(self):
-	# Get the mixer
-	self.mixer = amixer.Mixer()
-	self.mixer.attach()
-	self.mixer.load()
-	#print self.mixer.list()
-	self._get_mixer_elements()
-	self.mute_all()
-	
-    def _get_mixer_elements(self):
-	# Get the mixer elements
-	if self.mixer.count > 0:
-	    elements = self.mixer.list()
-	    for i in elements:
-                if i[0] == :
-	            self._set_master(i)
-		elif i[0] == 'PCM':
-		    self._set_pcm(i)
-		elif i[0] == 'Capture':
-		    self._set_capture(i)
-
-	# TODO mic & capture
-
-    def _set_master(self, element):
-	self.master = amixer.Element(self.mixer, element[0], element[1])
-
-    def _set_pcm(self, element):
-	self.pcm = amixer.Element(self.mixer, element[0], element[1])
-
-    def _set_capture(self, element):
-	self.capture = amixer.Element(self.mixer, element[0], element[1])
-
-    def _set_input_src(self, element):
-	self.input_src = amixer.Element(self.mixer, element[0], element[1])
-
-    def _set_mic(self, element):
-	self.mic = amixer.Element(self.mixer, element[0], element[1])
-
-    def _set_mic_boost(self, element):
-	self.mic_bost = amixer.Element(self.mixer, element[0], element[1])
-
-    #def _set_capture(self, element):
-    #	self.capture = amixer.Element(self.mixer, element[0], element[1])
+	_Vss.__init__(self)
 
     def mute_all(self):
 	if self.master:
             self.master.set_volume_all(0)
-
-    def mute_mic(self, state=True):
-	pass
-
-    def mute_phone(self, state=True):
-	pass
-
-    def mute_speaker(self, state=True):
-	pass
-
-    def open_mic(self):
-	pass
-
-    def stereo_on(self, state=False):
-	pass
-
-    def migGain_down(self):
-	pass
-
-    def micGain_up(self):
-	pass
-
-    def phoneVol_up(self):
-	pass
-
-    def phoneVol_down(self):
-        pass
-
-    def speakerVol_up(self):
-	pass
-
-    def speakerVol_down(self):
-	pass
-
 
 
