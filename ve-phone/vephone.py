8d0c7998ccb7f8c8cf6318db529bc658e1d38d23
Ernesto Celis <developer@celisdelafuente.net>
2014-02-07 16:38:44 -0600

    * DBG dialing problem. MIC & Exit works, not dialing

--- ve-phone/vephone.py
+++ ve-phone/vephone.py
@@ -39,7 +39,6 @@ def main_loop():
             # Search the address book first, it only handles 0 to 9
             for contact in address_book:
                 # address_book = {'1': ('1001', 'AMBULANCE'), ...}
-                print contact
                 if choice == str(contact):
                     uri = "sip:"+address_book[contact][0]+"@"+sipcfg['srv']
                     syslog.syslog(syslog.LOG_INFO, "SCK Dial "
