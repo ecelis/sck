f16fc289c071a5c200f3cc0fa38abc3e11c40ab5
Ernesto Celis <developer@celisdelafuente.net>
2014-02-07 01:29:28 -0600

    * UPDATE PBX registration status logging codes added for 300 failed reg.

--- ve-phone/vephone.py
+++ ve-phone/vephone.py
@@ -95,12 +95,10 @@ class VeAccountCallback(pj.AccountCallback):
 
     def on_reg_state(self):
 	if self.sem:
-            if self.account.info().reg_status >= 300:
+            if self.account.info().reg_status >= 200:
                 syslog.syslog(syslog.LOG_ERR, 'SIP registration ' +
                         str(self.account.info().reg_status) + ' ' +
                         self.account.info().reg_reason)
-            if self.account.info().reg_status >= 200:
-		syslog.syslog(syslog.LOG_INFO,"SIP registration " + str(self.account.info().reg_status) + " " + self.account.info().reg_reason)
 		self.sem.release()
 
     def on_incoming_call(self, call):
