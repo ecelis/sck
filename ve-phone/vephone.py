1d8dcff6a15e30eb41f0ee6a198245a5c532cbb0
Ernesto Celis <developer@celisdelafuente.net>
2014-02-11 17:52:54 -0600

    * FIX branch with no sql supportt,temporary.

--- ve-phone/vephone.py
+++ ve-phone/vephone.py
@@ -32,6 +32,7 @@ def log_cb(level, str, len):
 def main_loop():
     while True:
         syslog.syslog(syslog.LOG_INFO, "SCK Ready!")
+
         try:
             # Read only one character from standard input
             getch = gc._Getch()
@@ -86,34 +87,43 @@ def make_call(uri):
 
 """ Callback for handling registration on PBX """
 class VeAccountCallback(pj.AccountCallback):
-    
     sem = None
 
     def __init__(self, account):
         pj.AccountCallback.__init__(self, account)
 
+
     def wait(self):
-	self.sem = threading.Semaphore(0);
-	self.sem.acquire()
+        self.sem = threading.Semaphore(0);
+        self.sem.acquire()
+
 
     def on_reg_state(self):
         if self.sem:
             if self.account.info().reg_status >= 200:
-                syslog.syslog(syslog.LOG_ERR, 'SCK registration status ' + str(self.account.info().reg_status) + ' ' + self.account.info().reg_reason)
-
+                syslog.syslog(syslog.LOG_ERR,
+                        'SCK registration status ' +
+                        str(self.account.info().reg_status) + ' ' +
+                        self.account.info().reg_reason
+                )
+            
             self.sem.release()
+
 
     def on_incoming_call(self, call):
 	#TODO A lot of stuff, call handling mainly and logging also
-        syslog.syslog(syslog.LOG_INFO, "SCK Incoming call from "
-	    + call.info().remote_uri)
-	global current_call
+        syslog.syslog(syslog.LOG_INFO, "SCK Incoming call from " + 
+                call.info().remote_uri
+        )
+        global current_call
         #TODO global tone
-	#tone = VeTone().ring_start()
+        #tone = VeTone().ring_start()
         current_call = call
-	call_cb = VeCallCallback(current_call)
-	current_call.set_callback(call_cb)
-	current_call.answer(200)
+        call_cb = VeCallCallback(current_call)
+        current_call.set_callback(call_cb)
+        current_call.answer(200)
+
+
 
 """ Class to receive events from Call """
 class VeCallCallback(pj.CallCallback):
@@ -121,57 +131,66 @@ class VeCallCallback(pj.CallCallback):
     def __init__(self, call=None):
         pj.CallCallback.__init__(self, call)
 
-    """ Notification when call sate has changed """
+
     def on_state(self):
-	global current_call
-        syslog.syslog(syslog.LOG_INFO,"SCK Call is " + self.call.info().state_text)
-        syslog.syslog(syslog.LOG_INFO, "    Last code = " + str(self.call.info().last_code))
-        syslog.syslog(syslog.LOG_INFO, "    (" + self.call.info().last_reason + ")")
+        global current_call
+        syslog.syslog(syslog.LOG_INFO,
+                "SCK Call is " + self.call.info().state_text
+        )
+        syslog.syslog(syslog.LOG_INFO, " Last code = " +
+                str(self.call.info().last_code)
+        )
+        syslog.syslog(syslog.LOG_INFO,
+                " (" + self.call.info().last_reason + ")"
+        )
 
         global call_state
-	call_state = self.call.info().state
+        call_state = self.call.info().state
 
         #global tone
-	if call_state == pj.CallState.EARLY:
-#	    tone = VeTone().ring_start()
+        if call_state == pj.CallState.EARLY:
+            #tone = VeTone().ring_start()
             pass
-	elif call_state == pj.CallState.DISCONNECTED:
-	    #VeTone().ring_stop(tone)
+        elif call_state == pj.CallState.DISCONNECTED:
+            #VeTone().ring_stop(tone)
             current_call = None
 
     # Notification when call's media state changed
     def on_media_state(self):
         global lib
-#	global tone
+	    #global tone
         if self.call.info().media_state == pj.MediaState.ACTIVE:
             # Connect the call to sound device
-#            VeTone().ring_stop(tone)
+            #VeTone().ring_stop(tone)
             call_slot = self.call.info().conf_slot
             lib.conf_connect(call_slot, 0)
             lib.conf_connect(0, call_slot)
 
+
 class VeTone:
     def ring_start(self):
-	global tone
-	tone = lib.create_player(
-	    os.path.dirname(os.path.realpath(__file__)) + "/sounds/tone.wav",
-	    True
-	)
-	lib.conf_connect(tone, 0)
-	return tone
+        global tone
+        tone = lib.create_player(os.path.dirname(
+            os.path.realpath(__file__)) + "/sounds/tone.wav",
+            True
+        )
+        lib.conf_connect(tone, 0)
+        return tone
+
 
     def ring_stop(self, tone):
-	lib.conf_disconnect(tone, 0)
+        lib.conf_disconnect(tone, 0)
         lib.player_destroy(tone)
 
+
     def incoming(self):
-	global tone
-	tone = lib.create_player(
-	    os.path.dirname(os.path.realpath(__file__)) + "/sounds/three-tones.wav",
-	    False
-	)
-	lib.conf_connect(tone, 0)
-	return tone
+        global tone
+        tone = lib.create_player(
+                os.path.dirname(os.path.realpath(__file__)) +
+                "/sounds/three-tones.wav", False
+	    )
+        lib.conf_connect(tone, 0)
+        return tone
 
 
 try:
@@ -189,19 +208,23 @@ try:
     lib = pj.Lib()
     # Init pjsua with default config
     lib.init(log_cfg = pj.LogConfig(level=LOG_LEVEL, callback=log_cb))
-   # Create UDP transport which listens to any available port
+    # Set sound device TODO in veconfig.py
+    lib.set_snd_dev(-1,-1)
+    # Create UDP transport which listens to any available port
     transport = lib.create_transport(pj.TransportType.UDP)
     # Start the library
     lib.start()
+
     if sipcfg == None:
         # Create local/user-less account
         acc = lib.create_account_for_transport(transport)
     else:
         # Register on PBX
         acc = lib.create_account(
-	    pj.AccountConfig(sipcfg['srv'], 
-	    sipcfg['ext'], 
-	    sipcfg['pwd']))
+                pj.AccountConfig(sipcfg['srv'], sipcfg['ext'],
+                    sipcfg['pwd'])
+        )
+
     # Set the account call back
     acc_cb = VeAccountCallback(acc)
     acc.set_callback(acc_cb)
@@ -214,7 +237,7 @@ try:
     sys.exit(0)
 
 except pj.Error, e:
-    syslog.syslog(syslog.LOG_ERR,"SCK Exception: " + str(e))
+    syslog.syslog(syslog.LOG_ERR, "SCK Exception: " + str(e))
     lib.destroy()
     lib = None
     sys.exit(1)
