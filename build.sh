6b3e7717454ae784f6a9f7d548135b581345dbe1
Ernesto Celis <developer@celisdelafuente.net>
2014-03-21 10:58:02 -0600

    * UPDATE build.sh to delete files left by make distclean

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 make distclean
+rm -f pjmedia/include/pjmedia/config_auto.h
+rm -f pjmedia/include/pjmedia-codec/config_auto.h
+rm -f pjmedia/build/os-auto.mak
+rm -f pjlib/include/pj/config_site.h 
+rm -f pjlib/include/pj/compat/os_auto.h
+rm -f pjlib/include/pj/compat/m_auto.h
+rm -f pjlib/build/os-auto.mak
+rm -f pjlib-util/build/os-auto.mak
+rm -f build/os-auto.mak
+rm -f build/cc-auto.mak
+rm -f build.mak
 ./configure --disable-video --disable-ffmpeg --disable-v4l2
 CFLAGS="-fPIC" CXXFLAGS="-fPIC" make dep
 CFLAGS="-fPIC" CXXFLAGS="-fPIC" make 
